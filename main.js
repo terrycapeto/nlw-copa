7a1675c5a27323469223441efc4fd0f2e56ed478
terrycapeto <capetotrader@gmail.com>
2022-11-04 12:15:47 -0300

    add games

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-function createGame(player, hour, player2) {
+function createGame(group, player, hour, player2) {
     return `
-    <li>            
+    <p>${group}</p>  
+    <li>                    
         <img src="./assets/${player}.svg" alt="Bandeira do ${player}">
             <strong>${hour}</strong>   
-        <img src="./assets/${player2}.svg" alt="Bandeira da ${player2}">        
-    </li>
+        <img src="./assets/${player2}.svg" alt="Bandeira da ${player2}">
+           
+    </li><hr>
    `
 }
+
 let delay = -0.4;
 
 function createCard(date, day, games) {
@@ -20,7 +23,21 @@ function createCard(date, day, games) {
   </div>
   `
 }
-document.querySelector("#cards").innerHTML = createCard("28/11", "sexta", createGame("yemen", "16:00", "serbia") + createGame("brazil", "16:00", "serbia")) + createCard("14/11", "segunda", createGame("brazil", "16:00", "serbia")) + createCard("14/11", "segunda", createGame("brazil", "16:00", "serbia"))
+document.querySelector("#cards").innerHTML = 
+createCard("20/11", "Domingo", createGame("Grupo A","qatar", "13:00", "ecuador")) + 
+
+createCard("21/11", "segunda", createGame("Grupo B","England", "10:00", "iran") + createGame("Grupo A","senegal", "13:00", "netherlands")+ createGame("Grupo B", "united states", "16:00", "wales")) + 
+
+createCard("22/11", "Terça",createGame("Grupo C","argentina", "07:00", "saudi arabia") + createGame("Grupo D","denmark", "10:00", "tunisia") +createGame("Grupo C","mexico", "13:00", "poland") +createGame("Grupo D","france", "16:00", "australia")) +
+
+createCard("23/11", "Quarta",createGame("Grupo F","morocco", "07:00", "croatia") + createGame("Grupo E","germany", "10:00", "japan") +createGame("Grupo E","spain", "13:00", "costa rica") + createGame("Grupo F","belgium", "16:00", "canada")) +
+
+createCard("24/11", "Quinta",createGame("Grupo G","switzerland", "07:00", "cameroon") + createGame("Grupo H","uruguay", "10:00", "south korea") +createGame("Grupo H","portugal", "13:00", "ghana") + createGame("Grupo G","brazil", "16:00", "serbia")) +
+
+createCard("25/11", "Sexta",createGame("Grupo B","wales", "07:00", "iran") + createGame("Grupo A","qatar", "10:00", "senegal") +createGame("Grupo A","netherlands", "13:00", "ecuador") + createGame("Grupo B","england", "16:00", "united states")) 
+
+
+
 document.getElementById('radio-yellow').addEventListener('click', function() {
     document.querySelector('body').setAttribute('class', "yellow");
 })
